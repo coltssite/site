8ae373132724be7b21bbb4b0c64583523123c342
Colton Kawamura <coltonkawamura@Coltons-Macbook.local>
2020-05-08 08:00:33 -0700

    use hyde theme

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ PUBLIC_SITE_LOCAL_REPO="public/"
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
  
 # Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo -t hyde # if using a theme, replace with `hugo -t hyde`
  
 # Go To Public folder
 pushd "${PUBLIC_SITE_LOCAL_REPO}"
